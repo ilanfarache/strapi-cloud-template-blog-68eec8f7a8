04c94460a8f9c0699116a601137462a6062d6bc9
ilan farache <ilanfarache1994@gmail.com>
2025-07-04 11:58:22 +0300

    Implement catalog and category-press features, including new schemas, controllers, routes, and services. Update server configuration to include default app keys. Remove author schema and adjust media attributes in existing schemas for consistency.

---
+++ src/api/category-press/routes/category-press.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * category-press router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::category-press.category-press');
---
+++ src/api/category-press/services/category-press.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * category-press service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::category-press.category-press');
